b19af8f82e1bdf93f659507cef0c5802de9457c5
Jelena <jelenabatanjski@yahoo.com>
2023-10-03 09:21:59 +0200

    upload

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<script>
+
+    export default {
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            increase() {
+                this.count++
+            },
+            decrease() {
+                this.count--
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <p>{{ count }}</p>
+        <button v-on:click="increase">+</button>
+        <button v-on:click="decrease">-</button>
+    </div>
+</template>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    props: {
+        todos: {
+            type: Array
+        }
+    }
+}
+</script>
+
+<template>
+
+    <div>
+        <ol>
+            <li v-for="todo in todos">{{ todo }}</li>
+        </ol>
+    </div>
+
+</template>
